c4620d4e81c210ef5e3d5733876e5a7ed4269674
unknown <ibr.s.ahmat@gmail.com>
2022-08-20 02:10:30 +0300

    Edit the test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,23 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject {
-    User.new(name: "Anything", photo: 'http://twitter.com', bio: 'test for User')
-  }
+  
+  user = User.create(name: "Anything", photo: 'http://twitter.com', bio: 'test for User')
 
   describe "Tests for User model validations" do
     it "is not valid" do
-      expect(subject).to_not be_valid
+      expect(user).to_not be_valid
     end
 
     it "is not valid without a title" do
-      subject.posts_counter = 1
-      expect(subject).to be_valid
+      user.posts_counter = 1
+      expect(user).to be_valid
     end
 
     it "is not valid without a name" do
-      subject.name = nil
-      expect(subject).to_not be_valid
+      user.name = nil
+      expect(user).to_not be_valid
     end
   end
 end
