c42ccd0ef1fdcf96dfdd88e1585d79ccbe0ee998
unknown <ibr.s.ahmat@gmail.com>
2022-08-19 13:08:53 +0300

    Make required changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
-  belongs_to :user, foreign_key: 'user_id'
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
 
-  has_many :likes
-  has_many :comments
+  has_many :comments, class_name: 'Comment', foreign_key: 'user_id'
+  has_many :likes, class_name: 'Like', foreign_key: 'user_id'
 
   after_save :update_posts
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :posts
-  has_many :comments
-  has_many :likes
+  has_many :posts, class_name: 'Post', foreign_key: 'user_id'
+  has_many :comments, class_name: 'Comment', foreign_key: 'user_id'
+  has_many :likes, class_name: 'Like', foreign_key: 'user_id'
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
