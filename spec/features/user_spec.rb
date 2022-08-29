3e533faac52173706e8f70d11d85034d18510a4b
unknown <ibr.s.ahmat@gmail.com>
2022-08-30 00:53:34 +0300

    Fix failed tests

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -2,25 +2,25 @@ require 'rails_helper'
 
 RSpec.describe "User pages test index/show ", type: :feature do
   before(:all) do
-    @user = User.create(name: 'Integration test', photo: 'http://twitter.com', bio: 'test for User')
-    @post = Post.create(title: 'Rspec test', text: 'rspec test for post', user: @user)
-    @comment = Comment.create(post: @post, user: @user, text: 'This is a test for comment model')
-    @like = Like.create(post: @post, user: @user)
+    @first_user = User.create(name: 'Integration test', photo: 'http://twitter.com', bio: 'test for User')
+    @post = Post.create(title: 'Rspec test', text: 'rspec test for post', user: @first_user)
+    @comment = Comment.create(post: @post, user: @first_user, text: 'This is a test for comment model')
+    @like = Like.create(post: @post, user: @first_user)
   end
 
   describe 'User index page test' do
     scenario 'Should show the username ' do
       visit users_path
-      expect(page).to have_content(@user.name)
+      expect(page).to have_content(@first_user.name)
     end
 
     scenario 'Should show the profile picture' do
       visit users_path
-      expect(page).to have_selector("img[src='#{@user.photo}']")
+      expect(page).to have_selector("img[src='#{@first_user.photo}']")
     end
 
     scenario 'Should show the profile picture' do
-      @post = Post.create(title: 'Rspec test 2', text: 'rspec test for post', user: @user)
+      @post = Post.create(title: 'Rspec test 2', text: 'rspec test for post', user: @first_user)
       visit users_path
       expect(page).to have_text("Number of posts: 2")
     end
