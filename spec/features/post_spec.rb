27807f543ab9448eb3a87f02a662b091adab4ed6
sarazaToussaint <tmsaraza@gmail.com>
2022-08-30 14:04:36 +0200

    Add tests for post show page

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 # rubocop:disable Metrics/BlockLength
 RSpec.feature 'Posts', type: :feature do
   before(:all) do
-    @user1 = User.create(name: 'Integration test', photo: 'http://twitter.com', bio: 'test for User')
+    @user1 = User.create(name: 'Patrick Mboma', photo: 'http://twitter.com', bio: 'test for User')
+    @user2 = User.create(name: 'Paul Biya', photo: 'http://twitter.com', bio: 'test for User')
     @post1 = Post.create(title: 'Rspec test 1', text: 'rspec test for post', user: @user1)
-    @comment = Comment.create(user: @user1, post: @post1, text: 'test for User')
+    @comment1 = Comment.create(user: @user1, post: @post1, text: 'test for User')
+    @comment2 = Comment.create(user: @user2, post: @post1, text: 'test for User')
     @like = Like.create(user: @user1, post: @post1)
   end
 
@@ -58,6 +60,47 @@ RSpec.feature 'Posts', type: :feature do
       visit user_posts_path(@user1)
       click_on @post1.title
       expect(page).to have_current_path(user_post_path(@user1, @post1))
+    end
+  end
+
+  describe 'User post show page' do
+    scenario 'Should show the post title ' do
+      visit user_post_path(@user1, @post1)
+      expect(page).to have_content(@post1.title)
+    end
+
+    scenario 'Should show the username ' do
+      visit user_post_path(@user1, @post1)
+      expect(page).to have_content(@user1.name)
+    end
+
+    scenario 'Should show the number of comment ' do
+      visit user_post_path(@user1, @post1)
+      expect(page).to have_content("Comments: #{@post1.comments.count}")
+    end
+
+    scenario 'Should show the number of likes ' do
+      visit user_post_path(@user1, @post1)
+      expect(page).to have_content("Likes: #{@post1.likes.count}")
+    end
+
+    scenario 'Should show the post body ' do
+      visit user_post_path(@user1, @post1)
+      expect(page).to have_content(@post1.text)
+    end
+
+    scenario 'Should show the post commentor name ' do
+      visit user_post_path(@user1, @post1)
+      comment = @post1.comments.first
+      expect(page).to have_content(comment.author.name)
+    end
+
+    scenario 'Should show the comment each comment author left ' do
+      visit user_post_path(@user1, @post1)
+      comment = @post1.comments.first
+      comment2 = @post1.comments.last
+      expect(page).to have_content(comment.text)
+      expect(page).to have_content(comment2.text)
     end
   end
 end
