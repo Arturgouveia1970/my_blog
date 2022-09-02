81781797db9289b0cf4ffda9fb231ee3d72a569b
sarazaToussaint <tmsaraza@gmail.com>
2022-09-02 15:34:19 +0200

    Update the comment controller file

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  load_and_authorize_resource
+
   def create
     @post = Post.includes(:user).find(params[:post_id])
     @comment = Comment.create(comment_params)
@@ -14,6 +16,9 @@ class CommentsController < ApplicationController
 
   def destroy
     @comment = Comment.find(params[:id])
+    @post = Post.find(@comment.post_id)
+    @post.comments_counter -= 1
+    @post.save
     @comment.destroy!
     flash[:success] = 'You have deleted your comment!!.'
     redirect_to user_post_path(current_user, params[:post_id])
