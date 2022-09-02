95184d1ec7b3b5c9966c65c1b84d04b3b92c1a86
unknown <ibr.s.ahmat@gmail.com>
2022-09-02 16:11:14 +0300

    add delete_all in post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
 
-  has_many :comments, class_name: 'Comment', foreign_key: 'user_id', dependent: :destroy
-  has_many :likes, class_name: 'Like', foreign_key: 'user_id'
+  has_many :comments, class_name: 'Comment', foreign_key: 'user_id', dependent: :delete_all
+  has_many :likes, class_name: 'Like', foreign_key: 'user_id', dependent: :delete_all
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
