c51e9142c9b0df397f21813d53156d5a4863c560
unknown <ibr.s.ahmat@gmail.com>
2022-09-03 18:59:53 +0300

    Fix linter error

--- app/models/jwt_denylist.rb
+++ app/models/jwt_denylist.rb
@@ -1,7 +1,5 @@
-# rubocop:disable Layout/EndOfLine
 class JwtDenylist < ApplicationRecord
   include Devise::JWT::RevocationStrategies::Denylist
 
   self.table_name = 'jwt_denylist'
 end
-# rubocop:enable Layout/EndOfLine
