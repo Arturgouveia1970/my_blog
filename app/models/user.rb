77dca07e2dc30a6a2d2b62fc8a39625afab88b5f
sarazaToussaint <tmsaraza@gmail.com>
2022-09-03 17:38:55 +0200

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable, :confirmable,
+         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
   has_many :posts, class_name: 'Post', foreign_key: 'user_id'
   has_many :comments, class_name: 'Comment', foreign_key: 'user_id'
