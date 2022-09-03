66c0982fcbbfc773d98249e23ca989139326045e
unknown <ibr.s.ahmat@gmail.com>
2022-09-03 18:54:50 +0300

    Fix linter errors

--- app/models/jwt_denylist.rb
+++ app/models/jwt_denylist.rb
@@ -1,5 +1,7 @@
+# rubocop:disable Layout/EndOfLine
 class JwtDenylist < ApplicationRecord
   include Devise::JWT::RevocationStrategies::Denylist
 
   self.table_name = 'jwt_denylist'
 end
+# rubocop:enable Layout/EndOfLine
--- config/routes.rb
+++ config/routes.rb
@@ -17,9 +17,9 @@ Rails.application.routes.draw do
 
   namespace :api do 
     namespace :v1 do
-      resources :users, only: [index] do
-        resources :posts, only: [index, show] do
-          resources :comments, only: [new, create] do
+      resources :users, only:  %i[index] do
+        resources :posts, only:  %i[index show] do
+          resources :comments, only:  %i[new create] do
           end
         end
       end
